02277218e8b09924d601316306d2f53207638918
Shivam Shende <Shivamshende200@gmail.com>
2025-06-08 15:35:11 +0530

    update resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -19,8 +19,6 @@ const Resume = () => {
                 <h1>Resume</h1>
                 <div ref={resumeRef} className="resume-content">
                     <h2>Shivam Shende</h2>
-                    {/* <p><strong>Phone:</strong> +91 7263057752</p>
-                    <p><strong>Age:</strong> 23y</p> */}
                     <div className="contact-row">
                         <p><strong>Phone:</strong> +91 7263057752</p>
                         <p><strong>Age:</strong> 23y</p>
@@ -33,7 +31,7 @@ const Resume = () => {
 
                     <div className='adjective'>
                         <h2>Objective</h2>
-                        <p>I'm Shivam Shende, a Computer Engineering student started out as a web developer. But now, I want to gain more experience as a different role that allow me to gain more learnings where my skills can contribute like leadership, strategic thinking, management, and problem-solving.</p>
+                        <p>I'm Shivam Shende, I want to gain more experience as a role that allow me to gain more learnings where my skills can contribute like leadership, strategic thinking, management, and problem-solving.</p>
                     </div>
 
                     <h2>Education</h2>
@@ -41,7 +39,7 @@ const Resume = () => {
                     <p>Bachelor of Computer Science and Engineering</p>
 
                     <h2>Experience</h2>
-                    <p>Full Time Intern at <a href="https://anamarhs.com/" target="_blank">Anamarhs Web Solutions</a></p>
+                    <p>Full Time Intern at <a href="https://anamarhs.com/" target="_blank">Anamarhs</a></p>
                     <p>I've used skills during this job like,
                     <ul className='horizontal-list'>
                         <li>Communication</li>
@@ -72,7 +70,7 @@ const Resume = () => {
 
                     <p><em>Visit to <a href="https://shivam-shende-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">my Portfolio</a> to know more about me, my interest, my activity and most important <a href="https://medium.com/@shivamshende200" target="_blank" rel="noopener noreferrer">my Blogs</a> for my knowledge.</em></p>
 
-                    <p className='info'><em>I just need a job, no matter what role it is!</em></p>
+                    <p className='info'><em>Available for any role!!!</em></p>
                 </div>
             </div>
             <button className='download-resume' onClick={handlePrint}>Download Resume</button>
